df6d4ac646e960230a67cec474e17aff9764748a
Adnan RIHAN <adnan@rihan.fr>
2024-01-02 00:15:39 +0100

    Renamed students hotspot to hs-students

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('hotspot:clear hs-etudiants')
+        $schedule->command('hotspot:clear hs-students') // Todo: Set name in config file
                  ->daily()
                  ->runInBackground();
 
