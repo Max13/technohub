186f02cd00c73cd7be32cd60650780bf1ccb3ad3
Adnan RIHAN <adnan@rihan.fr>
2024-01-08 23:28:19 +0100

    Routes cleanup/sort

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\Auth\GoogleController;
 use App\Http\Controllers\Auth\YpareoController;
+use App\Http\Controllers\HotspotController;
 use App\Http\Controllers\Hotspot\StaffController;
 use App\Http\Controllers\Hotspot\StudentController;
-use App\Http\Controllers\HotspotController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,17 +20,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-// Hotspot
-Route::get('/hotspot/login', [HotspotController::class, 'redirectToLogin'])->name('hotspot.redirectToLogin');
-
+// Auth
 Route::get('/auth/google', [GoogleController::class, 'showLogin'])->name('auth.google.showLogin');
 Route::get('/auth/google/redirect', [GoogleController::class, 'redirect'])->name('auth.google.redirect');
 Route::get('/auth/google/callback', [GoogleController::class, 'callback'])->name('auth.google.callback');
-
-Route::get('/hotspot/staff/callback', [StaffController::class, 'callback'])->name('hotspot.staff.callback');
-
 Route::get('/auth/ypareo', [YpareoController::class, 'showLogin'])->name('auth.ypareo.showLogin');
 Route::post('/auth/ypareo', [YpareoController::class, 'doLogin'])->name('auth.ypareo.doLogin');
 
+// Hotspot
+Route::get('/hotspot/login', [HotspotController::class, 'redirectToLogin'])->name('hotspot.redirectToLogin');
+Route::get('/hotspot/staff/callback', [StaffController::class, 'callback'])->name('hotspot.staff.callback');
 Route::get('/hotspot/students/callback', [StudentController::class, 'callback'])->name('hotspot.students.callback');
 Route::get('/hotspot/connected', [HotspotController::class, 'showConnected'])->name('hotspot.showConnected');
