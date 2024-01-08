37ff1b7e702179880216c6101d3605a1724563de
Adnan RIHAN <adnan@rihan.fr>
2024-01-08 23:16:23 +0100

    Welcome route/view

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
 // Hotspot
 Route::get('/hotspot/login', [HotspotController::class, 'redirectToLogin'])->name('hotspot.redirectToLogin');
