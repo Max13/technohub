109261430e59c488ee1f70d5f5bc9f6cfe9a8f0e
Adnan RIHAN <adnan@rihan.fr>
2024-01-14 23:36:31 +0100

    Random UserAgent for Ypareo check because of Captive UA being always the same

--- app/Http/Controllers/Auth/YpareoController.php
+++ app/Http/Controllers/Auth/YpareoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Services\Ypareo;
+use Faker\Generator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -67,7 +68,7 @@ class YpareoController extends Controller
             ]
         );
 
-        if ($ypareo->auth($data['username'], $data['password'], $request->userAgent())) {
+        if ($ypareo->auth($data['username'], $data['password'], app(Generator::class)->userAgent())) {
             $user = DB::table('users')->where('ypareo_login', $data['username'])->first();
 
             $request->session()->flash('auth.user', $user);
