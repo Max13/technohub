fa6820a0f494cce37c2b46d95a53af6186c40555
Adnan RIHAN <adnan@rihan.fr>
2024-03-08 07:41:40 +0100

    Integrate BugSnag to track errors

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,4 +36,15 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function bootstrappers()
+    {
+        return array_merge(
+            [\Bugsnag\BugsnagLaravel\OomBootstrapper::class],
+            parent::bootstrappers(),
+        );
+    }
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,4 +64,15 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function bootstrappers()
+    {
+        return array_merge(
+            [\Bugsnag\BugsnagLaravel\OomBootstrapper::class],
+            parent::bootstrappers(),
+        );
+    }
 }
