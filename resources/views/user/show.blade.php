263a3d63d93bf19a245addbf86db238525b06509
Chéridanh <divintsiela@gmail.com>
2024-11-05 13:07:07 +0100

    adding views and resource controller for user

---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,11 @@
+@php($title = "New collaborateur")
+
+@extends('layouts.app')
+
+@section('bodyClass', 'd-flex align-items-center py-4 bg-body-tertiary')
+
+@section('content')
+    <main class="container text-center">
+        <h1 class="mb-5">Store</h1>
+    </main>
+@endsection
