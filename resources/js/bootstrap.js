47a180198618710b7b6301a1f65eea11439345a3
Adnan RIHAN <adnan@rihan.fr>
2024-11-22 07:47:15 +0100

    Added Bootstrap 5 Autocomplete plugin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,3 +26,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+// Bootstrap-5-autocomplete
+window.Autocomplete = require('bootstrap-5-autocomplete').Autocomplete;
