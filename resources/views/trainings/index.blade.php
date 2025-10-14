4068c21da5cb224a48cee2a9bae1b82c3aa767a9
Chéridanh <divintsiela@gmail.com>
2025-10-14 22:53:32 +0200

    feat(redisign) : update training index view

--- resources/views/trainings/index.blade.php
+++ resources/views/trainings/index.blade.php
@@ -12,6 +12,38 @@
             </div>
         </div>
 
+        <div class="row">
+            @foreach ($trainings as $training)
+                <div class="col-xl-3 col-sm-6 py-2">
+                    <div class="card h-100 hover:shadow">
+                        <a class="text-decoration-none text-reset" href="{{ route('trainings.show', $training) }}">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar-md">
+                                        <div class="avatar-title bg-soft-primary text-primary display-6 m-0 rounded-circle">
+                                            <i class="bi bi-people p-3"></i>
+                                        </div>
+                                    </div>
+                                    <div class="flex-1 ms-3">
+                                        <h5 class="font-size-16 mb-1">
+                                            {{ $training->name }}
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div class="mt-3 pt-1">
+                                    <P class="text-muted mb-0">
+                                        <i class="bi bi-info-circle font-size-15 align-middle pe-2 text-primary"></i>
+                                        <span class="badge badge-soft-success mb-0">{{ $training->students->count() }} {{ __('Students') }}</span>
+                                    </P>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+        <!--
         <div class="row row-cols-1 row-cols-md-3 g-4 my-4">
             @foreach ($trainings as $training)
                 <div class="col">
@@ -26,6 +58,7 @@
                 </div>
             @endforeach
         </div>
+        -->
     </main>
 @endsection
 
