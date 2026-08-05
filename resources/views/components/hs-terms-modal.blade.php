f502752a18ac905a3d1a1d55bd6394093188ade6
Adnan RIHAN <adnan@rihan.fr>
2026-08-05 22:40:00 +0200

    Typo

--- resources/views/components/hs-terms-modal.blade.php
+++ resources/views/components/hs-terms-modal.blade.php
@@ -5,7 +5,7 @@
                 <h1 id="termsModalLabel">Conditions Générales d'Utilisation</h1>
             </div>
             <div class="modal-body">
-                <p class="small text-muted">Mise à jour: 21/12/2023</p>
+                <p class="small text-muted">Mise à jour: 05/08/2026</p>
 
                 <h2>1. Introduction</h2>
                 <p>L'accès à Internet à <strong>ITIC Paris</strong> via ce portail est soumis aux conditions générales d'utilisation énoncées ci-dessous. En utilisant ce service, vous acceptez de respecter ces conditions.</p>
@@ -21,7 +21,7 @@
 
                 <h2>5. Accès</h2>
                 <p>Vous êtes responsable de la sécurité de votre compte d'accès à Internet. Ne partagez pas vos identifiants de connexion.</p>
-                <p>Vous pouvez vous connecter à tout moment, et avec autant d'appareils que vous souhaitez. Toutes les sessions étudiantes sont réinitialisées à <u>19h00</u>, au delà il vous sera demandé de nouveau de vous connecter.</p>
+                <p>Vous pouvez vous connecter à tout moment, et avec autant d'appareils que vous souhaitez. Toutes les sessions étudiantes sont réinitialisées à <u>19h00</u>, au delà il vous sera demandé à nouveau de vous connecter.</p>
 
                 <h2>6. Sanctions en Cas de Non-Respect</h2>
                 <p>Le non-respect des présentes conditions peut entraîner la suspension ou la résiliation de votre accès. Des sanctions disciplinaires ou légales supplémentaires peuvent également être appliquées en cas de violation grave des règles.</p>
@@ -30,7 +30,7 @@
                 <p>L'établissement se réserve le droit de modifier ces conditions générales à tout moment et sans avertissement. Il est de la responsabilité des utilisateurs de consulter régulièrement ces conditions.</p>
 
                 <h2>8. Contact</h2>
-                <p>Pour toute question légale ou technique, ou préoccupation concernant ces conditions générales d'utilisation, ou l'utilisation du portail, veuillez contacter le service informatique de l'établissement en <u>Salle 14</u>, par e-mail (<a href="mailto:tech@iticparis.com">tech@iticparis.com</a>) ou par téléphone au <a href="tel:+33143736271">01&nbsp;43&nbsp;73&nbsp;62&nbsp;71</a>.</p>
+                <p>Pour toute question légale ou technique, ou préoccupation concernant ces conditions générales d'utilisation, ou l'utilisation du portail, veuillez contacter le service informatique de l'établissement au <u>Salle 11</u>, par e-mail (<a href="mailto:tech@iticparis.com">tech@iticparis.com</a>) ou par téléphone au <a href="tel:+33143736271">01&nbsp;43&nbsp;73&nbsp;62&nbsp;71</a>.</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal">{{ __('Close') }}</button>
